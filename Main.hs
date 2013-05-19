61360756f363cbf4a531333b6feedf7fe3b93d0c
Mark Borkum <m.i.borkum@soton.ac.uk>
2013-05-19 15:36:26 +0100

    Re-factored executable

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import qualified Data.Attoparsec.ByteString
 import qualified Data.Attoparsec.Text
 import qualified Data.ByteString.Char8 as B8
 import           Data.Canonical (canonicalize)
-import           Data.Described (describeWith)
+import           Data.Described (Described(describeWith))
 import qualified Data.DescriptorTree as D
 import qualified Data.Set as S
 import qualified Data.Text as T
@@ -21,8 +21,6 @@ import           Text.PrettyPrint.HughesPJClass (pPrint, render, Doc, (<+>), cha
 import JSON2RDF
 
 data JSON2RDF = Transform { file_ :: FilePath }
-              | Show      { file_ :: FilePath }
-              | Canon     { file_ :: FilePath }
               | Describe  { file_ :: FilePath , intersection_ :: Bool , pretty_ :: Bool }
                 deriving (Eq, Show, Data, Typeable)
 
@@ -32,12 +30,8 @@ mode =
     $ modes [ Transform { file_ = fileFlags def
                         } &= help "Transform JSON document(s) into RDF graph"
                           &= auto
-            , Show      { file_ = fileFlags def
-                        } &= help "Print s-expression version of named input FILE"
-            , Canon     { file_ = fileFlags def
-                        } &= help "Print canonical version of named input FILE"
             , Describe  { file_         = fileFlags def
-                        , intersection_ = def &= name "I" &= help "Intersection (instead of union)"
+                        , intersection_ = def &= name "I" &= help "Use \"intersection\" operator (instead of \"union\")"
                         , pretty_       = def &= name "P" &= help "Format output using pretty-printer"
                         } &= help "Print description of JSON document structure"
             ] &= program "json2rdf"
@@ -52,7 +46,6 @@ main = do
   
   text <- Data.Text.IO.readFile (file_ args)
   let expr = fmap canonicalize (Data.Attoparsec.Text.parseOnly expr' text)
-  
   either (\l -> hPutStrLn stderr l >> exitFailure) (\r -> go1 args r >> exitSuccess) expr
   
     where
@@ -60,22 +53,34 @@ main = do
       go1 :: JSON2RDF -> Expression -> IO ()
       go1 (Transform _) =
         forever . go2
-      go1 (Show _) =
-        putStrLn . show
-      go1 (Canon _) =
-        putStrLn . render . pPrint
-      go1 (Describe _ bool1 bool2) =
-        putStrLn . render . D.pp_DescriptorTree bool2 . (if bool1
-          then describeWith D.intersection (maxBound :: D.DescriptorTree T.Text ())
-          else describeWith D.union (minBound :: D.DescriptorTree T.Text ()))
+      go1 (Describe _ bool1 bool2) = do
+        putStrLn_pp_DescriptorTree bool2 . opts_describeWith bool1
       
       go2 :: Expression -> IO ()
       go2 expr = do
-        bs <- B8.getLine
-        let v = either (const Nothing) Just (Data.Attoparsec.ByteString.parseOnly value' bs)
-        time <- getCurrentTime
-        let ts = js2rdf (Just time) expr v
-        mapM_ (putStrLn . render . pp_RDFTriple) (S.toList ts)
+        currentTime <- getCurrentTime
+        currentLine <- B8.getLine
+        let v = Data.Attoparsec.ByteString.parseOnly value' currentLine
+        let ts = js2rdf (Just currentTime) expr (eitherToMaybe v)
+        putStrLn_pp_RDFTriple ts
+      
+      eitherToMaybe :: Either a b -> Maybe b
+      eitherToMaybe =
+        either (const Nothing) Just
+      
+      opts_describeWith :: Bool -> Expression -> D.DescriptorTree T.Text ()
+      opts_describeWith True =
+        describeWith D.intersection maxBound
+      opts_describeWith False =
+        describeWith D.union minBound
+      
+      putStrLn_pp_DescriptorTree :: (Show k, Show v) => Bool -> D.DescriptorTree k v -> IO ()
+      putStrLn_pp_DescriptorTree b =
+        putStrLn . render . D.pp_DescriptorTree b
+      
+      putStrLn_pp_RDFTriple :: S.Set RDFTriple -> IO ()
+      putStrLn_pp_RDFTriple =
+        mapM_ (putStrLn . render . pp_RDFTriple) . S.toList
       
       pp_RDFTriple :: RDFTriple -> Doc
       pp_RDFTriple (s, p, o) =
